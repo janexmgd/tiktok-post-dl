b1539b03bd81b9b9c137ae3c7a4cd6fd4f85548c
janexmgd <janexmgd@gmail.com>
2023-12-28 13:40:56 +0700

    updating bulk downloader must efficient

--- src/core/bulkDownloader.js
+++ src/core/bulkDownloader.js
@@ -1,30 +1,18 @@
 // import { readList } from '../utils/fileUtils.js';
 import inquirer from 'inquirer';
 import downloader from '../tools/downloader.js';
-import delay from '../utils/simpleDelay.js';
 import client from '../tools/client.js';
 import path from 'path';
 import fs from 'fs';
+import colors from '@colors/colors';
 const bulkDownloader = async (username) => {
   try {
-    // console.log(username);
-    // return;
-    const res = await client({
-      method: 'POST',
-      url: 'https://long-gold-crab-cuff.cyclic.app/feed/tiktok',
-      data: {
-        username: username,
-      },
+    const base_url = 'https://long-gold-crab-cuff.cyclic.app';
+    const userInfoResponse = await client({
+      method: 'GET',
+      url: `${base_url}/tiktok/user-info?username=${username}`,
     });
-    const data = res?.data?.data;
-    if (!data) {
-      throw new Error('error when fetching profile');
-    }
-    const userInfo = {
-      username: data.username,
-      nickname: data.nickname,
-      feedLength: data.feedLength,
-    };
+    const userInfo = userInfoResponse.data.data;
     console.log(userInfo);
     const asq = await inquirer.prompt([
       {
@@ -37,17 +25,40 @@ const bulkDownloader = async (username) => {
       console.log('process stopped by user');
       return;
     }
+    console.log(`${colors.green('!')} start fetching user feed`);
+    const secUid = userInfoResponse.data.data.secUid;
+
+    let cursor = 0;
+    let hasMore = true;
+    const postList = [];
+
+    while (hasMore) {
+      const userFeedResponse = await client({
+        method: 'GET',
+        url: `${base_url}/tiktok/user-feed?secUid=${secUid}&cursor=${cursor}`,
+      });
+      const {
+        hasMore: feedHasMore,
+        cursor: feedCursor,
+        itemList,
+      } = userFeedResponse.data.data;
+
+      hasMore = feedHasMore;
+      cursor = feedCursor;
+
+      postList.push(...itemList);
+    }
+    console.log(`${colors.green('!')} success fetching feed ${username}`);
     const outputPath = path.join(process.cwd(), 'media', username);
     if (!fs.existsSync(path.join(process.cwd(), 'media'))) {
       fs.mkdirSync(path.join(process.cwd(), 'media'));
     }
-    console.log(`file will saved at ${outputPath}`);
-    const feedList = data.feedList;
-    for (const i in feedList) {
+    console.log(`! file will saved at ${outputPath}`);
+    for (const i in postList) {
       const num = parseInt(i);
       const current = num + 1;
-      const e = feedList[i];
-      await downloader(username, e.url, current, userInfo.feedLength);
+      const e = postList[i];
+      await downloader(username, e.url, current, postList.length);
     }
   } catch (error) {
     console.error(error);
